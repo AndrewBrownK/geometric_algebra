0bb1600b36e04251f76f14feed0f99d78cc4a25d
abrown <a.jagged.b@gmail.com>
2024-05-21 17:41:09 -0700

    Polish spirv example

--- rga3d/src/shaders.rs
+++ rga3d/src/shaders.rs
@@ -5,12 +5,19 @@
 // https://github.com/AndrewBrownK/projective_ga/
 //
 
+use std::fs;
+use std::path::Path;
+
+use naga::back::spv::{Options, PipelineOptions, WriterFlags, ZeroInitializeWorkgroupMemoryMode};
+use naga::valid::{Capabilities, ValidationFlags, Validator};
+use naga::ShaderStage;
+
 /// Include the full wgsl source file (which may be several megabytes in size) in your rust binary.
 /// It is recommended to compose and prune your shaders during your app build, instead of your app
 /// runtime. (Hint: Enable this feature in [build-dependencies], but not [dependencies].)
 /// Despite this recommendation, you can still include this in your app binary if you really want
 /// or need to recompile shaders at app runtime for some reason.
-#[cfg(feature = "include-wgsl-src")]
+#[cfg(feature = "wgsl-compose")]
 pub const RGA3D_WGSL_SRC: &str = include_str!("shaders/rga3d.wgsl");
 
 /// Include the full glsl source file (which may be several megabytes in size) in your rust binary.
@@ -18,10 +25,10 @@ pub const RGA3D_WGSL_SRC: &str = include_str!("shaders/rga3d.wgsl");
 /// runtime. (Hint: Enable this feature in [build-dependencies], but not [dependencies].)
 /// Despite this recommendation, you can still include this in your app binary if you really want
 /// or need to recompile shaders at app runtime for some reason.
-#[cfg(feature = "include-glsl-src")]
+#[cfg(feature = "glsl-compose")]
 pub const RGA3D_GLSL_SRC: &str = include_str!("shaders/rga3d.glsl");
 
-#[cfg(feature = "include-wgsl-src")]
+#[cfg(feature = "wgsl-compose")]
 pub fn wgsl_composable_module_descriptor() -> naga_oil::compose::ComposableModuleDescriptor<'static> {
     naga_oil::compose::ComposableModuleDescriptor {
         source: RGA3D_WGSL_SRC,
@@ -31,24 +38,9 @@ pub fn wgsl_composable_module_descriptor() -> naga_oil::compose::ComposableModul
     }
 }
 
-#[cfg(feature = "include-wgsl-src")]
+#[cfg(feature = "wgsl-compose")]
 pub fn wgsl_compose_with_entrypoints(naga_module_descriptor: naga_oil::compose::NagaModuleDescriptor) -> Result<naga::Module, naga_oil::compose::error::ComposerError> {
     let mut composer = naga_oil::compose::Composer::default();
-    // Long story for turning off validation:
-    // - Composer.add_composable_module has a validation branch
-    // - That validation branch will use naga to turn the Naga Internal Representation of the header
-    //   into a string. The problem is that whenever naga does this, it renames things willy-nilly
-    //   like it is totally harmless. The biggest offenses are in proc::namer.sanitize, where it
-    //   adds underscores to names that end in digits and gets rid of double underscores in
-    //   identifiers (which are actually EXTREMELY NECESSARY for readability in our case).
-    // - So even though the Naga IR has correct identifiers, the wgsl string output of this IR
-    //   does not. And so validation fails, because the identifiers are not the same before and
-    //   after naga has mangled them
-    // - So if we skip validation, the Naga IR stays correct, and we just hope everything is okay.
-    // - Then if you really want to do validation, you can do it after composition instead of
-    //   during composition.
-    // TODO verify the post-composition validation actually does work.
-    // composer.validate = false;
     composer.add_composable_module(wgsl_composable_module_descriptor()).unwrap();
     let mut naga_module = composer.make_naga_module(naga_module_descriptor)?;
     let mut pruner = naga_oil::prune::Pruner::new(&naga_module);
@@ -59,7 +51,43 @@ pub fn wgsl_compose_with_entrypoints(naga_module_descriptor: naga_oil::compose::
     return Ok(naga_module);
 }
 
-#[cfg(feature = "include-glsl-src")]
+/// Compose wgsl, validate the module, and output a SPIR-V file for each entry point.
+/// Half for utility, half for example, a pattern like this is useful in build.rs.
+/// If you'd like to customize any of the options, you can copy and/or inline this function.
+#[cfg(feature = "wgsl-compose")]
+pub fn wgsl_compose_validate_and_spirv<P: AsRef<Path>, S: Into<String>>(wgsl_file_path: &str, spirv_outputs: Vec<(P, S, ShaderStage)>) -> anyhow::Result<()> {
+    let shader_src = fs::read_to_string(wgsl_file_path)?;
+    let naga_module_descriptor = naga_oil::compose::NagaModuleDescriptor {
+        source: shader_src.as_str(),
+        file_path: wgsl_file_path,
+        ..Default::default()
+    };
+    let naga_module = wgsl_compose_with_entrypoints(naga_module_descriptor)?;
+    let validator_flags = ValidationFlags::default();
+    let capabilities = Capabilities::default();
+    let mut validator = Validator::new(validator_flags, capabilities);
+    let naga_module_info = validator.validate(&naga_module)?;
+    let options = Options {
+        lang_version: (1, 6),
+        flags: WriterFlags::empty(),
+        binding_map: Default::default(),
+        capabilities: None,
+        bounds_check_policies: Default::default(),
+        zero_initialize_workgroup_memory: ZeroInitializeWorkgroupMemoryMode::Native,
+        debug_info: None,
+    };
+    for (spirv_path, entry_point, shader_stage) in spirv_outputs {
+        let pipeline_options = PipelineOptions {
+            shader_stage,
+            entry_point: entry_point.into(),
+        };
+        let spv = naga::back::spv::write_vec(&naga_module, &naga_module_info, &options, Some(&pipeline_options))?;
+        fs::write(spirv_path, bytemuck::cast_slice(spv.as_slice()))?;
+    }
+    Ok(())
+}
+
+#[cfg(feature = "glsl-compose")]
 pub fn glsl_composable_module_descriptor() -> naga_oil::compose::ComposableModuleDescriptor<'static> {
     naga_oil::compose::ComposableModuleDescriptor {
         source: RGA3D_GLSL_SRC,
@@ -69,13 +97,13 @@ pub fn glsl_composable_module_descriptor() -> naga_oil::compose::ComposableModul
     }
 }
 
-#[cfg(feature = "include-glsl-src")]
+#[cfg(feature = "glsl-compose")]
 pub fn glsl_compose_with_entrypoints(naga_module_descriptor: naga_oil::compose::NagaModuleDescriptor) -> Result<naga::Module, naga_oil::compose::error::ComposerError> {
     let mut composer = naga_oil::compose::Composer::default();
     composer.add_composable_module(glsl_composable_module_descriptor()).unwrap();
     let mut naga_module = composer.make_naga_module(naga_module_descriptor)?;
 
-    // TODO pruning seems to corrupt glsl modules, giving us absurd/incorrect
+    // Pruning seems to corrupt glsl modules, giving us absurd/incorrect
     //  "Initializer doesn't match the variable type" validation errors
     // let mut pruner = naga_oil::prune::Pruner::new(&naga_module);
     // for ep in naga_module.entry_points.iter() {
@@ -85,7 +113,3 @@ pub fn glsl_compose_with_entrypoints(naga_module_descriptor: naga_oil::compose::
 
     return Ok(naga_module);
 }
-
-// #[cfg(feature = "glsl-out")]
-// #[cfg(feature = "spv-out")]
-// #[cfg(feature = "wgsl-out")]
