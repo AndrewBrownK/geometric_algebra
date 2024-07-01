8002dcdbc09d5a9c60e17391e8fd4748db717c22
abrown <a.jagged.b@gmail.com>
2024-07-01 02:49:26 -0700

    Explicitly define full MultiVector

--- codegen/src/build_scripts2/cga3d.rs
+++ codegen/src/build_scripts2/cga3d.rs
@@ -1,5 +1,5 @@
 use crate::{ga, multi_vec, multi_vecs};
-
+use crate::algebra2::basis::generators::GeneratorElement::e3;
 
 /// Lengyel styled CGA of 5 dimensions representing 3 dimensions
 #[test]
@@ -22,13 +22,20 @@ pub fn cga3d_script() {
         Line        as e415, e425, e435 | e235, e315, e125;
         Plane       as e4235, e4315, e4125, e3215;
 
-        RoundPoint  as e1, e2, e3, e4 | e5;
+        RoundPoint  as e1, e2, e3, | e4, e5;
         Dipole      as e41, e42, e43 | e23, e31, e12 | e15, e25, e35, e45;
         Circle      as e423, e431, e412, e321 | e415, e425, e435 | e235, e315, e125;
-        Sphere      as e1234 | e4235, e4315, e4125, e3215;
+        Sphere      as e4235, e4315, e4125 | e1234, e3215;
 
         Motor       as e415, e425, e435, e12345 | e235, e315, e125;
         Flector     as e15, e25, e35, e45 | e4235, e4315, e4125, e3215;
+
+        MultiVector as
+            scalar, e12345 |
+            e1,e2,e3 | e4, e5 |
+            e41, e42, e43 | e23, e31, e12 | e15, e25, e35, e45 |
+            e423, e431, e412, e321 | e415, e425, e435 | e235, e315, e125 |
+            e4235, e4315, e4125 | e1234, e3215
     );
 
 
